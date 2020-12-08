babfca2cd70ed57f3afcb95c0c3a65227632a9cc
codeburnout <75696721+codeburnout@users.noreply.github.com>
2020-12-08 22:39:49 +0200

    mac address blacklisting support

--- scandevices.sh
+++ scandevices.sh
@@ -4,7 +4,7 @@
 
 iface="eth1"					# network interface for scanning the devices
 gonesec=90					# mark device away if hasn't seen for x seconds
-iprange="192.168.10.10-192.168.10.200"
+iprange="192.168.10.10-192.168.10.200"		# hacklab dhcp range
 
 #####
 
@@ -14,15 +14,28 @@ declare -A devices				# all devices, mac address as a key, timestamp as a value
 declare -A ipslist				# we need IP addresses too
 declare -A status				# current status of the device: "here" or "" if gone/undefined
 
+if [ ! -f macblacklist ]
+then
+	touch macblacklist
+fi
+
 while [ 0 ]; do
 	# ARP-scan devices between $iprange and parse its results:
 	nearcount=0
 	while read i; do
-		((nearcount=nearcount+1))
 		ip=$(echo $i|awk '{print $1}')
 		mac=$(echo $i|awk '{print $2}')
-		devices[$mac]=$(date +%s)					# update last seen
-		ipslist[$mac]=$ip
+
+		if ! grep -qi "^$mac" macblacklist; then
+			((nearcount=nearcount+1))
+			devices[$mac]=$(date +%s)		# update device's last seen
+			ipslist[$mac]=$ip
+		else
+			if [ -n "${devices[$i]}" ]; then
+				unset devices[$mac]
+				unset ipslist[$mac]
+			fi
+		fi
 	done < <(arp-scan --timeout=1500 --retry=2 --ignoredups --numeric --plain --quiet --interface=$iface $iprange)
 
 	# Track status changes:
@@ -31,14 +44,14 @@ while [ 0 ]; do
 
 		echo -n "$i  |  IP:${ipslist[$i]}  |  Lastseen:${lastseen}s  |  Status:${status[$i]} "
 
-		if [ "${status[$i]}" == "" ]; then				# status so far "away"
-			if (( $lastseen <= 2 )); then				# but was seen again
+		if [ "${status[$i]}" == "" ]; then		# device responded to arp scan, change status from "away" to "near"
+			if (( $lastseen <= 2 )); then
 				echo -n "Arrived!"
 				status[$i]="Near"
 				#curl https://127.0.0.1/here/$i
 			fi
 		else
-			if (( $lastseen >= $gonesec )); then			# lately near but now device hasn't responded to arp-scans for over $gonesec
+			if (( $lastseen >= $gonesec )); then	# lately "near" device hasn't responded to arp-scans for over $gonesec, make it go "away"
 				if ping -q -n -W3 -c2 ${ipslist[$i]} >/dev/null 2>&1; then
 					echo -n "Ping says still here!?"
 				else
